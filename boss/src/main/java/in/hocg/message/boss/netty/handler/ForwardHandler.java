e37c3f872450900379454a2fb130c5e55cb419a5
hocgin <hocgin@gmail.com>
2019-03-06 23:00:56 +0800

    ++ 无依赖

---
+++ boss/src/main/java/in/hocg/message/boss/netty/handler/ForwardHandler.java
@@ -0,0 +1,31 @@
+package in.hocg.message.boss.netty.handler;
+
+import in.hocg.message.boss.netty.ioc.Invoker;
+import in.hocg.message.boss.netty.ioc.InvokerManager;
+import in.hocg.message.boss.netty.message.packet.AbstractPacket;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.Optional;
+
+/**
+ * Created by hocgin on 2019/3/6.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@ChannelHandler.Sharable
+public class ForwardHandler extends SimpleChannelInboundHandler<AbstractPacket> {
+    public static final ForwardHandler INSTANCE = new ForwardHandler();
+    
+    private ForwardHandler() {
+    
+    }
+    
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, AbstractPacket msg) throws Exception {
+        Optional<Invoker> invokerOptional = InvokerManager.getInvoker(msg.getModule(), msg.getCommand());
+        invokerOptional.ifPresent(invoker -> invoker.invoke(ctx, msg));
+    }
+}
